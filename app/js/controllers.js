7559311b3584fd3f8d8f38bbf3650f0fa360895b
Marc Von Ahn <marc.vonahn@adp.com>
2013-11-14 13:13:05 -0800

    progress

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,6 +25,44 @@ angular.module('cctApp.controllers', []).
             }
         };
 
+        $scope.tableRowExpanded = false;
+        $scope.tableRowIndexExpandedCurr = "";
+        $scope.tableRowIndexExpandedPrev = "";
+        $scope.schoolIdExpanded = "";
+
+        $scope.schoolRowCollapseFn = function () {
+            $scope.schoolRowCollapse = [];
+            for (var i = 0; i < $scope.schools.school.contacts.length; i += 1) {
+                $scope.schoolRowCollapse.push(false);
+            }
+        };
+        $scope.selectTableRow = function (index, schoolId) {
+            if (typeof $scope.schoolRowCollapse === 'undefined') {
+                $scope.schoolRowCollapseFn();
+            }
+
+            if ($scope.tableRowExpanded === false && $scope.tableRowIndexExpandedCurr === "" && $scope.storeIdExpanded === "") {
+                $scope.tableRowIndexExpandedPrev = "";
+                $scope.tableRowExpanded = true;
+                $scope.tableRowIndexExpandedCurr = index;
+                $scope.storeIdExpanded = schoolId;
+                $scope.schoolRowCollapse[index] = true;
+            } else if ($scope.tableRowExpanded === true) {
+                if ($scope.tableRowIndexExpandedCurr === index && $scope.storeIdExpanded === schoolId) {
+                    $scope.tableRowExpanded = false;
+                    $scope.tableRowIndexExpandedCurr = "";
+                    $scope.storeIdExpanded = "";
+                    $scope.schoolRowCollapse[index] = false;
+                } else {
+                    $scope.tableRowIndexExpandedPrev = $scope.tableRowIndexExpandedCurr;
+                    $scope.tableRowIndexExpandedCurr = index;
+                    $scope.storeIdExpanded = schoolId;
+                    $scope.schoolRowCollapse[$scope.tableRowIndexExpandedPrev] = false;
+                    $scope.schoolRowCollapse[$scope.tableRowIndexExpandedCurr] = true;
+                }
+            }
+
+        };
   })
   .controller('MyCtrl2', [function() {
 
